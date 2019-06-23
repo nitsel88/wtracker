fc1a3662219850129a0b08169f7704c6270f4bdd
nitsel88 <nitsel88@gmail.com>
2019-06-23 23:39:37 +0530

    forworking
    On branch master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent }  from './employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees/new', component: EmployeeComponent },
+  { path: 'employees/:empId', component: EmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      EmployeeComponent
+   ],
+   imports: [
+      BrowserModule, 
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
